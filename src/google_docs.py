aa4c4df2826c1b352f18e02c91d3f262dd4fb67c
tanweshapasupalak6oly <tanweshapasupalak@gmail.com>
2026-07-06 17:55:44 +0530

    Improve Google Docs creation

--- src/google_docs.py
+++ src/google_docs.py
@@ -1,27 +1,22 @@
-"""Create a Google Doc for the weekly AI report."""
+"""Create and format Google Docs reports."""
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-import json
-import os
+import json, os
 
-SCOPES = [
-    "https://www.googleapis.com/auth/documents",
-    "https://www.googleapis.com/auth/drive.file",
-]
+SCOPES=["https://www.googleapis.com/auth/documents","https://www.googleapis.com/auth/drive"]
 
 
-def get_services():
-    info = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"])
-    creds = service_account.Credentials.from_service_account_info(info, scopes=SCOPES)
-    docs = build("docs", "v1", credentials=creds)
-    drive = build("drive", "v3", credentials=creds)
-    return docs, drive
+def _services():
+    info=json.loads(os.environ['GOOGLE_SERVICE_ACCOUNT_JSON'])
+    creds=service_account.Credentials.from_service_account_info(info,scopes=SCOPES)
+    return build('docs','v1',credentials=creds), build('drive','v3',credentials=creds)
 
 
-def create_report(title: str, body: str):
-    docs, _ = get_services()
-    doc = docs.documents().create(body={"title": title}).execute()
-    doc_id = doc["documentId"]
-    docs.documents().batchUpdate(documentId=doc_id, body={"requests":[{"insertText":{"location":{"index":1},"text":body}}]}).execute()
+def create_report(title:str, body:str):
+    docs, drive = _services()
+    doc=docs.documents().create(body={'title':title}).execute()
+    doc_id=doc['documentId']
+    docs.documents().batchUpdate(documentId=doc_id,body={'requests':[{'insertText':{'location':{'index':1},'text':body}}]}).execute()
+    drive.permissions().create(fileId=doc_id,body={'type':'anyone','role':'reader'}).execute()
     return doc_id
